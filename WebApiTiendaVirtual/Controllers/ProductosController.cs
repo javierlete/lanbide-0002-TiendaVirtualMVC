34a39dd0d5c8856ca1335f9f6da96014e59ea073
Javier Lete <javier@eskura21.com>
2021-10-28 11:26:16 +0200

    WebApi de Consultar funcionando

---
+++ WebApiTiendaVirtual/Controllers/ProductosController.cs
@@ -0,0 +1,18 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiTiendaVirtual.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        public IEnumerable<Producto> Get()
+        {
+            return Bll.ProductosBll.Consultar();
+        }
+    }
+}
