4268101cdb5690dd61ba06f05d9dc63b4ac09945
Javier Lete <javier@eskura21.com>
2021-10-29 09:11:45 +0200

    Página "SPA" (Single Page Application) que consume la WebAPI por AJAX

---
+++ WebApiTiendaVirtual/js/index.js
@@ -0,0 +1,44 @@
+﻿const URL = '/api/productos/'
+
+// ES5
+//window.onload = function() {
+//    fetch(URL)
+//        .then(function (respuesta) {
+//            return respuesta.json();
+//        })
+//        .then(function (productos) {
+//            console.log(productos);
+//        });
+//};
+
+// ES2015
+//window.onload = () => {
+//    fetch(URL)
+//        .then((respuesta) => respuesta.json())
+//        .then((productos) => console.log(productos));
+//};
+
+// ES2017
+window.onload = async () => {
+    const respuesta = await fetch(URL);
+    const productos = await respuesta.json();
+
+    console.log(productos);
+
+    let tr;
+
+    const tbody = document.getElementsByTagName('tbody')[0];
+
+    for (const producto of productos) {
+        console.log(producto);
+
+        tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>${producto.Id}</th>
+            <td>${producto.Nombre}</th>
+            <td>${producto.Precio}</th>
+            <td>${producto.FechaCaducidad}</th>`;
+
+        tbody.appendChild(tr);
+    }
+}
