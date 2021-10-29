1fcee8b6b80c1bb7411e97461d6916244ce36bf6
Javier Lete <javier@eskura21.com>
2021-10-29 09:19:21 +0200

    Aplicación de consola

---
+++ PlConsola/Program.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productos = Bll.ProductosBll.Consultar();
+
+            foreach (var producto in productos)
+            {
+                Console.WriteLine(producto);
+            }
+        }
+    }
+}
